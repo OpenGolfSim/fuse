00b160f975a7e0f47c040dbda4e39f549c810484
Brian Robinson <brian@ndmweb.com>
2026-05-26 12:59:34 -0600

    fix: clean up server and module entry

--- scripts/server.js
+++ scripts/server.js
@@ -7,44 +7,12 @@ import * as cheerio from 'cheerio';
 const PORT = process.env.PORT || 3112;
 const DIST_DIR = path.join(import.meta.dirname, '../dist');
 const PUBLIC_DIR = path.join(import.meta.dirname, '../public');
-const THREE_DIR = path.join(import.meta.dirname, '../node_modules/three/build');
 const viteConfigFile = 'vite.config.js';
 
 const app = express();
 
-function slashMiddleware(req, res, next) {
-  const ext = path.extname(req.path);
-  if (!ext && req.path.substr(-1) !== '/' && req.path.length > 1) {
-    const query = req.url.slice(req.path.length);
-    res.redirect(301, req.path + '/' + query);
-  } else {
-    next();
-  }
-}
-
 function createServer() {
-  // redirect directory urls to always end in /
-  // this ensures game assets come from their folder
-  app.use(slashMiddleware);
 
-  // Inject runtime scripts into example games
-  // this emulates how we inject the runtime into games within the OpenGolfSim app
-
-  // app.get('/games/:gameFolder/', async (req, res) => {
-  //   const pagePath = path.join(PUBLIC_DIR, 'games', req.params.gameFolder, 'index.html');
-  //   if (!fs.existsSync(pagePath)) {
-  //     return res.sendStatus(404);
-  //   }
-  //   const data = await fs.promises.readFile(pagePath);
-  //   const $ = cheerio.load(data.toString('utf-8'));
-  //   $('head').prepend([
-  //     `<link rel="stylesheet" type="text/css" href="/dist/runtime.css" />`,
-  //     `<script src="/dist/runtime.iife.js"></script>`
-  //   ].join(''));
-  //   res.send($.html());  
-  // });
-  
-  app.use('/three', express.static(THREE_DIR));
   // check public first to serve live changes
   app.use(express.static(PUBLIC_DIR));
   // fallback to dist, like production would use
@@ -52,14 +20,17 @@ function createServer() {
 
   return new Promise((resolve) => {
     app.listen(PORT, () => {
-      console.log(`Listening at http://localhost:${PORT}/`);
+      console.log('');
+      console.log('');
+      console.log(`OGS-FUSE is running at: http://localhost:${PORT}/`);
+      console.log('');
       resolve();
     });
   });
 }
 
 async function startWatchedBuild() {
-  console.log('Building module files...');
+  console.log('Building module...');
   const watcher = await build({
     configFile: viteConfigFile,
     build: { emptyOutDir: false, watch: {} },
--- vite.config.js
+++ vite.config.js
@@ -10,18 +10,14 @@ export default defineConfig({
   build: {
     lib: {
       name: 'OpenGolfSim_FUSE',
-      // entry: 'src/runtime.js',
-      // fileName: 'runtime',
-      // formats: ['iife'],  // self-executing — sets globals immediately on load
       entry: 'src/index.ts',
       fileName: 'fuse',
-      formats: ['es'],  // self-executing — sets globals immediately on load
+      formats: ['es'],
     },
-    // sourcemap: true,
+    sourcemap: true,
     outDir: 'dist',
     emptyOutDir: true,
     target: 'es2020',
-    minify: 'esbuild',
     rollupOptions: {
       output: {
         inlineDynamicImports: true,
